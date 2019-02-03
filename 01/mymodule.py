075b2acf749e3c6d9bba3feee05e2a27d2e934e7
Jeppe Druedahl <gmf123@ku.dk>
2019-02-03 20:15:33 +0100

    Firste lecture

--- 01/mymodule.py
+++ 01/mymodule.py
@@ -2,4 +2,5 @@ def myfunction(x):
     return x**2
 
 if __name__ == "__main__":
-    print('hellow world')
+    for i in range(10):
+        print(f'hello world: myfunction({i}) = {myfunction(i)}')
